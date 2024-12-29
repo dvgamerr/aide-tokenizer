bee5c84d4f6a4f3e28eef26b0c4c3b5ae7de93d6
Kananek Thongkam <info.dvgamer@gmail.com>
2024-12-29 22:43:20 +0700

    🚀feat(webhook): add LINE API utility functions for messaging and animations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import { Elysia } from "elysia"
-import { Pgmq } from "pgmq-js"
+import { Elysia } from 'elysia'
+// import { Pgmq } from 'pgmq-js'
+import { getChatId, preloadAnimation, pushMessage } from './providor/line' 
+const logger = require('pino')()
+
+
 
 const app = new Elysia()
-  .post('/webhook/:bot_id', async ({ body, params }) => {
+  .post('/:channel/:bot_name', async ({ body, params }) => {
+    const logs = logger.child({ id: body.events[0].message.id, destination: body.destination, ...params })
     try {
-      const queue = await Pgmq.queue('notice-queue')
-      await queue.send({ ...body, bot_id: params.bot_id })
+      logs.info('queue')
+      const chatId = getChatId(body.events[0])
+      // const queue = await Pgmq.queue('notice-queue')
+      // await queue.send({ ...body, bot_id: params.bot_id })
+      logs.info('preloading...')
+      preloadAnimation(chatId)
+      for await (const e of body.events) {
+        setTimeout(() => {
+          logs.info('push message...')
+          pushMessage(chatId, `${e.message.text}`)
+        }, 5000)
+      }
+
       return new Response(null, { status: 200 })
     } catch (error) {
       console.error('Webhook processing error:', error)
       return new Response(null, { status: 500 })
+    } finally {
+      logs.info('done')
     }
   })
   .listen(process.env.PORT || 3000)
 
-console.log(`Webhook server running on ${app.server?.hostname}:${app.server?.port}`)
+logger.info(`running on ${app.server?.hostname}:${app.server?.port}`)
---
+++ src/providor/line.js
@@ -0,0 +1,40 @@
+
+const accessToken = Bun.env.LINE_TOKEN
+const LINE_API = 'https://api.line.me/v2/bot'
+
+export const getChatId = e => e.source.type === 'user' ? e.source.userId : e.source.groupId || e.source.roomId
+
+export const preloadAnimation = async (chatId) => {
+  const res = await fetch(`${LINE_API}/chat/loading/start`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+    body: JSON.stringify({ chatId, loadingSeconds: 20 })
+  })
+
+  const data = await res.json()
+  if (!res.ok) throw new Error(data || `Failed ${res.statusText} (${res.status})`)
+
+  return data
+}
+
+export const pushMessage = async (userId, message) => {
+  const res = await fetch(`${LINE_API}/message/push`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+    body: JSON.stringify({ to: userId, messages: [{ type: 'text', text: message }] })
+  })
+
+  const data = await res.json()
+  if (!res.ok) {
+    console.log({ res, data })
+    throw new Error(data || `Failed ${res.statusText} (${res.status})`)
+  }
+
+  return data
+}
