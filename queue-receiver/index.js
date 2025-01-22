3e311f51ce17f019262f900fe105b86c31d946f5
Kananek Thongkam <info.dvgamer@gmail.com>
2025-01-22 15:16:18 +0700

    feat: streamline command handling and improve logging

--- queue-receiver/index.js
+++ queue-receiver/index.js
@@ -36,61 +36,43 @@ app.post('/_healthz', async () => {
 
 const isCommandIncluded = (body, cmd) => {
   for (const e of body.events) {
-    if (
-      e.message.type === 'text' &&
-      e.message.text.trim().toLowerCase().includes(cmd)
-    )
-      return true
+    if (e.message.type === 'text' && e.message.text.trim().toLowerCase() === `/${cmd}`) return true
     return false
   }
 }
 
 app.post('/:channel/:bot_name', async ({ body, params }) => {
-  if (
-    !valid_channels.includes(params.channel.toLowerCase()) ||
-    !valid_bots.includes(params.bot_name.toLowerCase()) ||
-    !body?.events.length
-  )
-    return new Response(null, { status: 404 })
+  if (!valid_channels.includes(params.channel.toLowerCase()) || !valid_bots.includes(params.bot_name.toLowerCase()) || !body?.events.length) return new Response(null, { status: 404 })
 
   const msgId = body.events[0].message.id
   let queueId = 0
   try {
     const chatId = getChatId(body.events[0])
-    const notice = await clientConn.query(
-      'SELECT token FROM notice WHERE name = $1 AND provider = $2',
-      [params.bot_name, params.channel.toUpperCase()],
-    )
+    const notice = await clientConn.query('SELECT token FROM notice WHERE name = $1 AND provider = $2', [params.bot_name, params.channel.toUpperCase()])
     const accessToken = notice.rows[0]?.token
     if (!accessToken) throw new Error("Bot doesn't have token")
 
-    if (isCommandIncluded(body, '/id')) {
-      await pushMessage(accessToken, chatId, `ID: ${chatId}`)
-      return new Response(null, { status: 200 })
-    }
-
-    if (isCommandIncluded(body, '/raw')) {
-      await pushMessage(accessToken, chatId, `ID: ${chatId}`)
-      return new Response(null, { status: 200 })
-    }
-
     // Query session_id by chatId
-    let res = await clientConn.query(
-      'SELECT session_id FROM sessions WHERE chat_id = $1 AND notice_name = $2',
-      [chatId, params.bot_name],
-    )
+    let res = await clientConn.query('SELECT session_id FROM sessions WHERE chat_id = $1 AND notice_name = $2', [chatId, params.bot_name])
     let sessionId = res.rows[0]?.session_id
     if (!res.rows.length) {
       // If not exists, generate random session_id and save to database
       sessionId = uuidv4()
-      await clientConn.query(
-        'INSERT INTO sessions (chat_id, notice_name, session_id) VALUES ($1, $2, $3)',
-        [chatId, params.bot_name, sessionId],
-      )
+      await clientConn.query('INSERT INTO sessions (chat_id, notice_name, session_id) VALUES ($1, $2, $3)', [chatId, params.bot_name, sessionId])
     }
 
     // await preloadAnimation(chatId, waitAnimation)
     for await (const event of body.events) {
+      if (isCommandIncluded(body, 'id')) {
+        await pushMessage(accessToken, chatId, `ID: ${chatId}`)
+        continue
+      }
+
+      if (isCommandIncluded(body, 'raw')) {
+        await pushMessage(accessToken, chatId, '```\n' + JSON.stringify(event) + '\n```')
+        continue
+      }
+
       queueId = await clientQueue.msg.send(queueName, {
         timestamp: event.timestamp,
         message: Object.assign(event.message, { replyToken: event.replyToken }),
--- queue-sender/index.js
+++ queue-sender/index.js
@@ -1,11 +1,17 @@
+import pino from 'pino'
+
 import { pgQueue, queueName } from '../provider/db'
 // import { getChatId, preloadAnimation, pushMessage } from '../provider/line'
 
+const logger = pino()
 const clientQueue = await pgQueue()
 
+logger.info(`Queue ${queueName} is running...`)
 while (true) {
   const sender = await clientQueue.msg.read(queueName, 10)
   if (!sender) continue
+  const { msgId, message } = sender
+  console.log(message)
 
-  console.log(sender)
+  await clientQueue.msg.delete(queueName, msgId)
 }
