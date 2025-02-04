415ca8af049c1516f212d02bcd322f461c764c82
Kananek Thongkam <info.dvgamer@gmail.com>
2025-02-04 15:14:15 +0700

    refactor: enhance error handling and improve query structure

--- queue-receiver/handler/botname-push.js
+++ queue-receiver/handler/botname-push.js
@@ -8,28 +8,34 @@ export default async (ctx) => {
   const text = query?.text || body?.text || !body?.altText
 
   try {
-    if (!apiKey) return new Response(null, { status: 401 })
+    if (!apiKey) {
+      return new Response(null, { status: 401 })
+    }
 
     const tokens = (text.length / 3.75).toFixed(0)
     logger.info(`[${botName}] ${tokens} tokens.`)
 
-    if (!text && !body?.messages?.length && !body?.contents?.length) return new Response(null, { status: 400 })
+    if (!text && !body?.messages?.length && !body?.contents?.length) {
+      return new Response(null, { status: 400 })
+    }
 
-    const notice = await db.query(
-      `
-        SELECT
-          n.access_token, u.chat_id, n.proxy,
-          coalesce(u.profile ->> 'displayName', u.chat_id) as display_name
-        FROM line_users u
-        INNER JOIN line_notice n ON n.name = u.notice_name
-        WHERE u.active = 't' AND u.notice_name = $1 AND u.api_key = $2 AND provider = 'LINE'
-      `,
-      [botName, apiKey],
-    )
+    const noticeQuery = `
+      SELECT
+        n.access_token, u.chat_id, n.proxy,
+        coalesce(u.profile ->> 'displayName', u.chat_id) as display_name
+      FROM line_users u
+      INNER JOIN line_notice n ON n.name = u.notice_name
+      WHERE u.active = 't' AND u.notice_name = $1 AND u.api_key = $2 AND provider = 'LINE'
+    `
+    const notice = await db.query(noticeQuery, [botName, apiKey])
 
-    if (!notice.rowCount) return new Response(null, { status: 401 })
+    if (!notice.rowCount) {
+      return new Response(null, { status: 401 })
+    }
 
     const { chat_id: chatId, proxy: proxyConfig, access_token: accessToken, display_name: displayName } = notice.rows[0]
+    const messages = body?.messages ? body.messages : [text ? { type: 'text', text } : body]
+
     await queueSend(
       {
         sessionId: null,
@@ -39,7 +45,7 @@ export default async (ctx) => {
         accessToken,
         proxyConfig,
       },
-      body?.messages ? body.messages : [text ? { type: 'text', text } : body],
+      messages,
     )
   } catch (ex) {
     logger.error(`[${botName}] ${ex.toString()}`)
