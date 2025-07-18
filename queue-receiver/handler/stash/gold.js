cb6924e99aedf5894e688acfc7eec845ca85cedf
Kananek Thongkam <info.dvgamer@gmail.com>
2025-07-18 18:25:18 +0700

    refactor: clean up code formatting and remove unused userAgent parameter

--- queue-receiver/handler/stash/gold.js
+++ queue-receiver/handler/stash/gold.js
@@ -1,21 +1,27 @@
 const GOLD_API = 'https://www.goldapi.io/api/XAU/USD'
-export default async ({ db, userAgent }) => {
+export default async ({ db }) => {
   const gold = await fetch(GOLD_API, {
     method: 'GET',
     headers: {
-      "Content-Type": "application/json",
-      "x-access-token": Bun.env.GOLD_API_KEY || ''
+      'Content-Type': 'application/json',
+      'x-access-token': Bun.env.GOLD_API_KEY || '',
     },
-    redirect: 'follow'
+    redirect: 'follow',
   })
-  const rates = await fetch("https://www.x-rates.com/calculator/?from=USD&to=THB&amount=1")
+  const rates = await fetch('https://www.x-rates.com/calculator/?from=USD&to=THB&amount=1')
 
   if (gold.ok && rates.ok) {
     const goldData = await gold.json()
     const ratesText = await rates.text()
 
-    const usd = parseFloat(ratesText.match(/USD =([\W\w]+?)THB/ig).join('').match(/[.\d]+/ig).join(''))
-    
+    const usd = parseFloat(
+      ratesText
+        .match(/USD =([\W\w]+?)THB/gi)
+        .join('')
+        .match(/[.\d]+/gi)
+        .join(''),
+    )
+
     await db.query(
       `INSERT INTO "stash"."gold" 
         ("tin", "tout", "tin_ico", "tout_ico", "usd_sale", "usd_buy", "update_at")
@@ -28,13 +34,12 @@ export default async ({ db, userAgent }) => {
         goldData.bid, // tout (price in grams)
         goldData.ch > 0 ? 'up' : 'down', // tin_ico
         goldData.ch > 0 ? 'up' : 'down', // tout_ico
-        isNaN(usd) ? 33 : usd,  // usd_sale
-        isNaN(usd) ? 33 : usd,  // usd_buy
+        isNaN(usd) ? 33 : usd, // usd_sale
+        isNaN(usd) ? 33 : usd, // usd_buy
         new Date(goldData.timestamp * 1000), // Convert Unix timestamp to Date object
       ],
     )
   }
-
 
   return new Response(JSON.stringify({ gold: gold.status, rates: rates.status }), { status: gold.status })
 }
